2e78a81690f9117efcbc5d4731b1c69c0ebbb0fb
Aryashu-1 <aryashu448@gmail.com>
2024-05-07 11:28:37 +0530

    webpage1

--- src/Components/Wa2/Wab.jsx
+++ src/Components/Wa2/Wab.jsx
@@ -1,14 +1,111 @@
-import React from 'react'
+    import React from 'react'
 
-const Wab = () => {
-  return (
-    <div>
-    <div className='flex m-auto border-2 border-black'>
-            <div><h1>helolo</h1></div>
-            <div></div>
+    const Wab = () => {
+    return (
+        <div className='h-screen w-full p-32 bg-gray-400'>
+        <div className='flex w-full justify-evenly '>
+
+        <div class="relative w-[350px] h-[320px] drop-shadow-2xl   rounded-xl bg-white mr-5 ">
+    <div class="absolute h-14 w-14 left-[40%] -top-10 ">  <img className=' fixed top-[-10] h-[70px] m-auto ' src="https://www.acpce.org/en/wp-content/uploads/2017/06/csi-logo.png" alt="" /></div>
+
+            
+                
+                    <h1 className='text-[#0d6efd] mt-12 text-[22px] font-semibold'>Computer Society of India</h1>
+                    <h1 className='text-sm mt-2 font-light'>Indias's Technical Student Chapter </h1>
+                    <div className='flex ml-32 mt-3'>
+                        <img className='h-[20px]' src="https://th.bing.com/th?id=OIP.V3N4LZrrSLNgG5hGB0kqCgHaHa&w=250
+    &h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"/>
+                        <h1 className='text-sm font-semibold '>VNRVJIET</h1>
+                    </div>
+                    <div className='flex ml-16 mt-2'>
+                        <div className='m-2'> 
+                            <h1 className='text-md font-semibold'>1.5M</h1>
+                            <h1 className=' text-sm '>Followers</h1>
+                        </div>
+                        <div className='m-2'> 
+                            <h1 className='text-md font-semibold'>1000+</h1>
+                            <h1 className=' text-sm '>Events</h1>
+                        </div>
+                        <div className='m-2'> 
+                            <h1 className='text-md font-semibold'>1M</h1>
+                            <h1 className=' text-sm '>Members</h1>
+                        </div>
+
+
+                    </div>
+                    <div className='flex my-2 mx-[120px]'>
+                        <img className='h-6 m-2' src="https://th.bing.com/th/id/OIP.t5xJJKNn9gCbzLc2q2rBBwHaHZ?w=184&h
+    =183&c=7&r=0&o=5&dpr=1.3&pid=1.7"/>
+    <img className='h-6 m-2' src="https://th.bing.com/th?id=OIP.d9YMY3jpZr_ZxliOr3xMYQHaHa&w=250
+    &h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2%22width=%2
+    225px"/>
+    <img className='h-6 m-2' src="https://th.bing.com/th?id=OIP.JCI8klaaA1w4LCtljpeJoAHaHa&w=250&h=
+    250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2%22width=%2230p
+    x"/>
+                        
+                    </div>
+                    <div>
+                        <button className='bg-[#0d6efd] px-4 py-1 mx-2 rounded-md text-white'>GO CSI</button>
+                        <button className='bg-[#0d6efd] px-4 py-1 mx-2 rounded-md text-white'>FOLLOW</button>
+                    </div>
+                    
+                </div>
+                <div>
+
+                </div>
+
+
+                <div class="relative w-[350px] h-[320px] drop-shadow-2xl   rounded-xl bg-white">
+    <div class="absolute h-14 w-14 left-[40%] -top-10 ">  <img className=' fixed top-[-10] h-[70px] m-auto ' src="https://img.collegepravesh.com/2021/06/VNR-VJIET-Hyderabad-Logo.png" alt="" /></div>
+
+            
+                
+                    <h1 className=' mt-12 text-[#d34c4c] font-semibold'>VNRVJIET</h1>
+                    <h1 className='text-sm mt-2 font-light'>Thamasoma Jyothirgamaya </h1>
+                    <div className='flex ml-32 mt-3'>
+                        <img className='h-[20px]' src="https://th.bing.com/th?id=OIP.V3N4LZrrSLNgG5hGB0kqCgHaHa&w=250
+    &h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"/>
+                        <h1 className='text-sm font-semibold '>Bachupally</h1>
+                    </div>
+                    <div className='flex ml-16 mt-2'>
+                        <div className='m-2'> 
+                            <h1 className='text-md font-semibold'>1995</h1>
+                            <h1 className=' text-sm '>Established</h1>
+                        </div>
+                        <div className='m-2'> 
+                            <h1 className='text-md font-semibold'>113</h1>
+                            <h1 className=' text-sm '>NIRF</h1>
+                        </div>
+                        <div className='m-2'> 
+                            <h1 className='text-md font-semibold'>A++</h1>
+                            <h1 className=' text-sm '>NAAC GRADE</h1>
+                        </div>
+
+
+                    </div>
+                    <div className='flex my-2 mx-[120px]'>
+                        <img className='h-6 m-2' src="https://th.bing.com/th/id/OIP.t5xJJKNn9gCbzLc2q2rBBwHaHZ?w=184&h
+    =183&c=7&r=0&o=5&dpr=1.3&pid=1.7"/>
+    <img className='h-6 m-2' src="https://th.bing.com/th?id=OIP.d9YMY3jpZr_ZxliOr3xMYQHaHa&w=250
+    &h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2%22width=%2
+    225px"/>
+    <img className='h-6 m-2' src="https://th.bing.com/th?id=OIP.JCI8klaaA1w4LCtljpeJoAHaHa&w=250&h=
+    250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2%22width=%2230p
+    x"/>
+                        
+                    </div>
+                    <div>
+                        <button className='bg-[#d34c4c] px-4 py-1 mx-2 rounded-md text-white'>GO VNR</button>
+                        <button className='bg-[#d34c4c] px-4 py-1 mx-2 rounded-md text-white'>FOLLOW</button>
+                    </div>
+                    
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
-    </div>
-  )
-}
+    )
+    }
 
-export default Wab
+    export default Wab
