ea3505dec114fc349d9acc15d080ed95bfa4fd50
Aryashu-1 <aryashu448@gmail.com>
2024-05-07 10:18:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex items-center text-center' >
+      <h1 className='text-3xl m-auto text-sky-400 '>Webalchemy</h1>
+    </div>
+  );
+}
+
+export default App;
